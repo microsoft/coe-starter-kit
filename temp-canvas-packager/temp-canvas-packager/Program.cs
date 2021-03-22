2744320d474eff824aa964b4123228c87c07f4d5
Michael Ochs <mike@mikefactorial.com>
2021-03-22 14:26:01 -0400

    Migrating Azure DevOps to GitHub public repo

---
+++ temp-canvas-packager/temp-canvas-packager/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.PowerPlatform.Formulas.Tools;
+using System.IO;
+
+namespace temp_canvas_packager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mode = args[0].ToLower();
+            if (mode == "-unpack")
+            {
+                string msAppPath = args[1];
+                string unpackedPath = args[2];
+                msAppPath = Path.GetFullPath(msAppPath);
+                (CanvasDocument msApp, ErrorContainer errors) = CanvasDocument.LoadFromMsapp(msAppPath);
+                msApp.SaveToSources(unpackedPath);
+            }
+            if (mode=="-pack")
+            {
+                string outFile = args[1];
+                string unpackedFolder = args[2];
+                (CanvasDocument msApp, ErrorContainer errors) = CanvasDocument.LoadFromSources(unpackedFolder);
+                msApp.SaveToMsApp(outFile);
+            }            
+        }
+    }
+}
