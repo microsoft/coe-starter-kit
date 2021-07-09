7dc1b21ad76bd7827ad0432e2242ca2d091d37f5
Grant Archibald <grant.archibald@microsoft.com>
2021-07-09 11:42:24 -0700

    Formatting updates

--- coe-cli/docs/build.ps1
+++ coe-cli/docs/build.ps1
@@ -1,4 +1,20 @@
+<#
+        .SYNOPSIS
+        Build the COE CLI E-book
+
+        .DESCRIPTION
+        Generate PDF version of the ebook from docs markdown files
+#>
+param ([string] $branch = "main")
 $path = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path))
 Write-Host $path
-coe ebook generate
+if ($branch.Length -gt 0) {
+    $isUri = ($NULL -ne $branch -as [System.URI]).AbsoluteURI
+    $url = $isUri ? $branch : "https://github.com/microsoft/coe-starter-kit/tree/${branch}/coe-cli/docs/"
+    Write-Host "Git Repository ${url}"
+    coe ebook generate -r $url
+} else {
+    coe ebook generate
+}
+
 docker run -it --rm -v ${path}:/docs cli-mdbook
--- coe-cli/docs/cli-development/adding-new-command.md
+++ coe-cli/docs/cli-development/adding-new-command.md
@@ -13,7 +13,7 @@ One you have unit test completed for your new command
 
 1. Review [https://www.npmjs.com/package/commander](https://www.npmjs.com/package/commander) on commands, options
 
-1. Update [commands.ts](..\..\src\commands\commands.ts) to include a new command or sub command
+1. Update [commands.ts](../../src/commands/commands.ts) to include a new command or sub command
 
 - Import your files at the top of the file
 
