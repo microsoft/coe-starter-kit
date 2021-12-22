e4c218e237dbc8155774df7dcf490a367969948b
Michael Ochs <mike@mikefactorial.com>
2021-12-22 08:25:13 -0500

    Test

--- coe-cli/src/commands/devops.ts
+++ coe-cli/src/commands/devops.ts
@@ -971,9 +971,7 @@ class DevOpsCommand {
         let devOpsOrgUrl = Environment.getDevOpsOrgUrl(args, args.settings)
         let baseUrl = `$(devOpsOrgUrl}${args.projectName}`
 
-        for (let t = 0; t < defaultAgent?.length; t++) {
-            this.logger?.info("Default Agent Pool: " + defaultAgent[t].id);
-        }
+        this.logger?.info("Agent Pool: " + defaultAgent?.length);
         let defaultAgentPool = defaultAgent?.length > 0 ? defaultAgent[0] : undefined
         this.logger?.info("Default Agent Pool: " + defaultAgentPool.id);
 
