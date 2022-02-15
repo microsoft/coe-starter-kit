7dadd24b86e89dddb0b6c5bc257e95f69cca4035
Johanna Lorenz <JohannaL@PPlatform.OnMicrosoft.com>
2022-02-15 12:02:30 -0500

    Fix for guarding against CanvasApps path not existing.

--- PowerShell/build-test-automation-urls.ps1
+++ PowerShell/build-test-automation-urls.ps1
@@ -1,32 +1,35 @@
 # Testable outside of agent
 function Set-CanvasTestAutomationURLs ($token, $url, $solutionName, $canvasAppsPath) {
     $filter = ".meta.xml"
-    $canvasApps = Get-ChildItem $canvasAppsPath -Filter "*$filter"
-    $asTestCase = "As TestCase"
     [System.Collections.ArrayList]$testUrls = @()
     $testUrlsObject = New-Object -TypeName PSObject
     $testUrlsObject | Add-Member -MemberType NoteProperty -Name TestURLs -Value $testUrls
-    $hostUrl = Get-HostFromUrl $url
 
-    foreach ($app in $canvasApps) {
-        $appName = $app.Name.Replace($filter, "")        
-        $appDirectory = $app.Directory.ToString()
-        $appSrcDirectory = "$appDirectory\$appName" + "_DocumentUri_msapp_src\Src\Tests"
-        $testFiles = Get-ChildItem $appSrcDirectory
+    if(Test-Path $canvasAppsPath) {
+        $canvasApps = Get-ChildItem $canvasAppsPath -Filter "*$filter"
+        $asTestCase = "As TestCase"
+        $hostUrl = Get-HostFromUrl $url
 
-        foreach ($testFile in $testFiles) {
-            $lines = Get-Content $testFile.FullName
+        foreach ($app in $canvasApps) {
+            $appName = $app.Name.Replace($filter, "")        
+            $appDirectory = $app.Directory.ToString()
+            $appSrcDirectory = "$appDirectory\$appName" + "_DocumentUri_msapp_src\Src\Tests"
+            $testFiles = Get-ChildItem $appSrcDirectory
 
-            foreach ($line in $lines) {                
-                if ($line.Contains($asTestCase)) {
-                    $testCaseId = $line.Split($asTestCase)[0].Replace("`"", "").Replace("'", "").Trim()
-                    $testUrl = Get-CanvasAppPlayUrl $token $hostUrl $appName
-                    $testUrl = "$testUrl&__PATestCaseId=$testCaseId&source=testStudioLink"
-                    $testUrls.Add($testUrl)
+            foreach ($testFile in $testFiles) {
+                $lines = Get-Content $testFile.FullName
+
+                foreach ($line in $lines) {                
+                    if ($line.Contains($asTestCase)) {
+                        $testCaseId = $line.Split($asTestCase)[0].Replace("`"", "").Replace("'", "").Trim()
+                        $testUrl = Get-CanvasAppPlayUrl $token $hostUrl $appName
+                        $testUrl = "$testUrl&__PATestCaseId=$testCaseId&source=testStudioLink"
+                        $testUrls.Add($testUrl)
+                    }
                 }
             }
         }
-    }    
+    }
     $json = ConvertTo-Json $testUrlsObject
     Set-Content -Path "CanvasTestAutomationURLs.json" -Value $json -Force
     Get-Content -Path "CanvasTestAutomationURLs.json"
