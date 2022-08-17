c2488886f4d6b5f43ead4fb1d22e5f9aebcbe29b
Mike Ochs <42348035+mikefactorial@users.noreply.github.com>
2022-08-17 14:02:08 -0400

    Test

--- coe-cli/src/commands/devops.ts
+++ coe-cli/src/commands/devops.ts
@@ -1175,9 +1175,8 @@ class DevOpsCommand {
                 await gitApi.getItemText(pipelineRepo.id, util.format("/Pipelines/build-deploy-%s-SampleSolution.yml", names[i]), args.projectName, null, null, null, null, null, version)
                     .then(function (response)
                     {
-                        response.on('readable', function () {
-                            content = response.read();
-                        });
+                        content = response.read();
+                        this.logger?.info(util.format("Content %s", content))
                     })
                     .catch(error => { this.logger?.error(util.format("Error getting pipeline file %s", error)); throw error })
                 this.logger?.info(util.format("Content %s", content))
