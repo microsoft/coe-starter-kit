c611c519e51fc1783e396cbbbe9e3384daa17c0a
Mike Ochs <42348035+mikefactorial@users.noreply.github.com>
2022-08-31 08:34:32 -0400

    Adding versioning and updating paths

--- coe-cli/scripts/package.ps1
+++ coe-cli/scripts/package.ps1
@@ -7,8 +7,12 @@
 #> Warning Failed to make bytecode node16-x64 for file /snapshot/coe-cli/node_modules/marked-terminal/node_modules/ansi-escapes/index.js
 #> Warning Failed to make bytecode node16-x64 for file C:\snapshot\coe-cli\node_modules\marked-terminal\node_modules\ansi-escapes\index.js
 # Replace the Copy-Item commands below with any distrubution commands you need to run to copy the files to the target location.
+node -p "'export const LIB_VERSION = ' + JSON.stringify(require('./package.json').version) + ';'" > src/version.ts
 npm run build
 npm link
 npx pkg -t linux,macos,win ./coe --out-path dist
-Copy-Item ./dist/coe-win.exe ../../coe-alm-accelerator-templates-azdo/Coe-Cli/
-Copy-Item ./dist/coe-linux ../../coe-alm-accelerator-templates-azdo/Coe-Cli/
+Copy-Item ./dist/coe-win.exe ../../coe-alm-accelerator-templates/Coe-Cli/coe-cli.exe
+Copy-Item ./dist/coe-linux ../../coe-alm-accelerator-templates/Coe-Cli/linux/coe-cli
+Copy-Item ./dist/coe-win.exe ../../coe-alm-accelerator-templates-azdo/Coe-Cli/coe-cli.exe
+Copy-Item ./dist/coe-linux ../../coe-alm-accelerator-templates-azdo/Coe-Cli/linux/coe-cli
+./dist/coe-win.exe --version
