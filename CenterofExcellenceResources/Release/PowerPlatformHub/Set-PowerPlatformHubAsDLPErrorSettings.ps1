1002edb42ff8b7c42e9c7c796f3ea18726a4b201
Daniel Laskewitz <dlaskewitz@microsoft.com>
2022-10-10 15:32:49 +0200

    Updated DLP script for #3871 & #3897. (#3903)
    Co-authored-by: Daniel Laskewitz <daniel.laskewitz@microsoft.com>

--- CenterofExcellenceResources/Release/PowerPlatformHub/Set-PowerPlatformHubAsDLPErrorSettings.ps1
+++ CenterofExcellenceResources/Release/PowerPlatformHub/Set-PowerPlatformHubAsDLPErrorSettings.ps1
@@ -1,40 +1,76 @@
 ## Set Variables for setting DLP error setting
-$newSiteURL = 'https://contoso.sharepoint.com/sites/powerplatformhub'
+$newSiteURL = 'https://contoso.sharepoint.com/sites/PowerPlatformHub'
 $supportEmail = 'support@contoso.com'
 $tenantId = '00000000-0000-0000-0000-000000000000'
 
-if ($psversiontable.PSVersion -ne [Version]'5.0') { 
+if ($psversiontable.PSVersion -eq [Version]'5.0' -or [Version]'5.1' ) { 
   
-  if (Get-Module -ListAvailable -Name PnP.PowerShell) {
-    Write-Host 'Microsoft.PowerApps.Administration.PowerShell module is already installed.'
+  $module = Get-Module -ListAvailable -Name Microsoft.PowerApps.Administration.PowerShell
+  $dlpPage = $newSiteURL + '/SitePages/Data-Loss-Prevention-(DLP)-Policies.aspx'
+
+  if ($module) {
+    Write-Host 'INFO: Microsoft.PowerApps.Administration.PowerShell module is already installed.'
+    if ($module.Version -lt [Version]'2.0.0') {
+      Write-Host 'INFO: Microsoft.PowerApps.Administration.PowerShell module is not up to date. Updating module...'
+      Update-Module -Name Microsoft.PowerApps.Administration.PowerShell
+    }
+    else {
+      Write-Host 'INFO: Microsoft.PowerApps.Administration.PowerShell module is up to date.'
+    }
   } 
   else {
-    Write-Host 'Microsoft.PowerApps.Administration.PowerShell module is not installed. Installing now...'
+    Write-Host 'INFO: Microsoft.PowerApps.Administration.PowerShell module is not installed. Installing now...'
     ## Install Module
     Install-Module -Name Microsoft.PowerApps.Administration.PowerShell
   }
   try {
-    Write-Host 'Connect to Power Platform'
+    Write-Host 'INFO: Connect to Power Platform'
     Add-PowerAppsAccount
   
-    Write-Host 'Adding site to DLP Error Settings'
-    New-PowerAppDlpErrorSettings -TenantId $tenantId -ErrorSettings @{
-      ErrorMessageDetails = @{
-        enabled = $True
-        url     = $newSiteURL
+    try {
+      Write-Host 'INFO: Get DLP error settings'
+      $errorSettings = Get-PowerAppDlpErrorSettings -TenantId $tenantId
+      if (!$errorSettings) {
+        ## Add site to DLP Error Settings if it doesn't exist
+        Write-Host 'INFO: DLP error settings not found. Adding DLP error settings...'
+        New-PowerAppDlpErrorSettings -TenantId $tenantId -ErrorSettings @{
+          ErrorMessageDetails = @{
+            enabled = $True
+            url     = $dlpPage
+          }
+          ContactDetails      = @{
+            enabled = $True
+            email   = $supportEmail
+          }
+        } | Out-Null
       }
-      ContactDetails      = @{
-        enabled = $True
-        email   = $supportEmail
+      else {
+        Write-Host 'INFO: DLP error settings already exist. Updating settings...'
+        Set-PowerAppDlpErrorSettings -TenantId $tenantId -ErrorSettings @{
+          ErrorMessageDetails = @{
+            enabled = $True
+            url     = $dlpPage
+          }
+          ContactDetails      = @{
+            enabled = $True
+            email   = $supportEmail
+          }
+        } | Out-Null
       }
     }
-    Write-Host 'Adding site to DLP Error Settings complete!' -ForegroundColor Green   
+    catch {
+      Write-Host 'ERROR: An error occurred during adding or updating the site to the DLP Error Settings.' -ForegroundColor Red
+      exit
+    } 
   }
   catch {
-    Write-Host 'An error occurred during adding the site to the DLP Error Settings.' -ForegroundColor Red
+    Write-Host 'ERROR: An error occurred during connecting to the Power Platform.' -ForegroundColor Red
+    exit
   }
-  Write-Host 'Script done! :)' -ForegroundColor Green
+  Write-Host 'SUCCESS: DLP Error Settings successfully updated or created.' -ForegroundColor Green
+  exit
 }
 else {
-  Write-Host "This script requires PowerShell 5.0 or higher. You are running $($psversiontable.PSVersion)." -ForegroundColor Red
+  Write-Host 'ERROR: This script requires PowerShell 5.0 or 5.1. You are running '$($psversiontable.PSVersion) -ForegroundColor Red
+  exit
 }
