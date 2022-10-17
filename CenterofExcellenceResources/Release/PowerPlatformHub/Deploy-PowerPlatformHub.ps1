f207a1a689bb3a94b4547cc8764f4d5cdfc94ab7
Daniel Laskewitz <dlaskewitz@microsoft.com>
2022-10-17 15:37:44 +0200

    Update to Power Platform Hub (#3972)
    Co-authored-by: Daniel Laskewitz <daniel.laskewitz@microsoft.com>

--- CenterofExcellenceResources/Release/PowerPlatformHub/Deploy-PowerPlatformHub.ps1
+++ CenterofExcellenceResources/Release/PowerPlatformHub/Deploy-PowerPlatformHub.ps1
@@ -9,18 +9,30 @@ $siteTemplate = 'SITEPAGEPUBLISHING#0'
 $siteTitle = 'Power Platform Hub'
 $timeZone = 2
 
-if (Get-Module -ListAvailable -Name PnP.PowerShell) { 
-  Write-Host 'INFO: PnP.PowerShell module is already installed.' 
+$module = Get-Module -ListAvailable -Name PnP.PowerShell
+
+if ($module) { 
+  Write-Host 'INFO: PnP.PowerShell module is already installed.'
+  if ($module.Version -lt [Version]'1.11.0') {
+    Write-Host 'INFO: PnP.PowerShell module is not up to date. Updating module...'
+    Update-Module -Name PnP.PowerShell
+  }
+  else {
+    Write-Host 'INFO: PnP.PowerShell module is up to date.'
+  }
 }
 else {
   ## Install Module
   Write-Host 'INFO: PnP.PowerShell module is not installed. Installing now...'
   Install-Module -Name PnP.PowerShell
 }
+
 try {
   ## Connect to SharePoint Online
   Write-Host 'INFO: Connecting to SharePoint Online'
+  Write-Host 'INFO: Connecting to' $adminURL
   Connect-PnPOnline -Url $adminUrl -Interactive
+  Write-Host 'INFO: Connected to' $adminURL
   
   try {
     # Check if site exists
@@ -34,7 +46,7 @@ try {
         Write-Host 'INFO: Creating new site...'
 
         ## Create new site
-        New-PnPTenantSite -Template $siteTemplate -Title $siteTitle -Url $newSiteUrl  -Lcid $lcid -Owner $ownerEmail -TimeZone $timeZone -Wait        
+        New-PnPTenantSite -Template $siteTemplate -Title $siteTitle -Url $newSiteUrl  -Lcid $lcid -Owner $ownerEmail -TimeZone $timeZone -Wait
       }
       else {
         Write-Host 'ERROR: Site is lingering in the recycle bin (Status = ' $recycledSiteStatus '). Remove the site from the recycle bin before running this script.' -ForegroundColor Red
@@ -43,18 +55,63 @@ try {
       }
     }
     else {
-      Write-Host 'ERROR: Site exist. Please remove the site or choose a different URL for the site.' -ForegroundColor Red
-      Write-Host 'ERROR: Deployment of Power Platform Hub failed!' -ForegroundColor Red
+      Write-Host 'INFO: Site already exists, skipping creation of site...'
+
+      ## Get current user
+      $ctx = Get-PnPContext
+      $ctx.Load($ctx.Web.CurrentUser)
+      $ctx.ExecuteQuery()
+      $currentUser = $ctx.Web.CurrentUser.Email
+
+      ## Add current user as site owner
+      Set-PnPTenantSite -Identity $newSiteURL -Owners @($ownerEmail, $currentUser)
+
+      Write-Host 'INFO: Connecting to site...'
+      Connect-PnPOnline -Url $newSiteURL -Interactive
+      Write-Host 'INFO: Connected to' $newSiteURL'.'
+
+      Write-Host 'INFO: Deploying template...'
+    
+      ## Get Date
+      $date = Get-Date
+      $year = $date.Year
+      $month = $date.Month + 1
+      $traceDate = Get-Date -Format 'yyyy-MM-dd'
+
+      ## Turn trace log on
+      Set-PnPTraceLog -On -LogFile $traceDate.txt
+
+      ## Import Template
+      Invoke-PnPSiteTemplate -Path template.pnp -Parameters @{'CompanyName' = $companyName; 'Year' = $year; 'Month' = $month } -ErrorAction Stop
+
+      Write-Host 'SUCCESS: Deployment of Power Platform Hub complete!' -ForegroundColor Green
+      Write-Host 'Go to the site:' $newSiteUrl -ForegroundColor Green
+
       exit
     }    
   }
   catch {
-    Write-Host 'ERROR: An error occurred during checks if site exists.' -ForegroundColor Red
+    Set-PnPTraceLog -Off
+    Write-Host 'ERROR: An error occurred.' -ForegroundColor Red
     Write-Host 'ERROR: Deployment of Power Platform Hub failed!' -ForegroundColor Red
     exit
   }
 
   try {
+    Write-Host 'INFO: Wait 15 seconds to make sure the site is ready...'
+    
+    ## Wait 15 seconds to make sure the site is ready
+    Start-Sleep -Seconds 15
+
+    ## Get current user
+    $ctx = Get-PnPContext
+    $ctx.Load($ctx.Web.CurrentUser)
+    $ctx.ExecuteQuery()
+    $currentUser = $ctx.Web.CurrentUser.Email
+
+    ## Add current user as site owner
+    Set-PnPTenantSite -Identity $newSiteURL -Owners @($ownerEmail, $currentUser)
+
     ## Connect to the newly created site
     Write-Host 'INFO: Connecting to newly created site'
     Connect-PnPOnline -Url $newSiteUrl -Interactive    
@@ -66,22 +123,22 @@ try {
   }
 
   try {
-    Write-Host 'INFO: Wait 15 seconds to make sure the site is ready...'
-    
-    ## Wait 15 seconds to make sure the site is ready
-    Start-Sleep -Seconds 15
-
     Write-Host 'INFO: Deploying template...'
     
     ## Get Date
     $date = Get-Date
     $year = $date.Year
     $month = $date.Month + 1
+    $traceDate = Get-Date -Format 'yyyy-MM-dd_HH:mm:ss'
+
+    ## Turn trace log on
+    Set-PnPTraceLog -On -LogFile $traceDate.txt
 
     ## Import Template
     Invoke-PnPSiteTemplate -Path template.pnp -Parameters @{'CompanyName' = $companyName; 'Year' = $year; 'Month' = $month } -ErrorAction Stop
   }
   catch {
+    Set-PnPTraceLog -Off
     Write-Host 'ERROR: Template deployment failed with error.' $Error -ForegroundColor Red
     Write-Host 'ERROR:' $Error -ForegroundColor Red
     Write-Host 'ERROR: Removing site collection...' -ForegroundColor Red
