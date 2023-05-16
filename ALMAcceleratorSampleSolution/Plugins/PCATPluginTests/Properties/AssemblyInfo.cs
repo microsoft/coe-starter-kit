d0084d613acd84c3596b20bcdcf0ba1d1d0be8c0
Rajeev Pentyala <104883923+RajeevPentyala@users.noreply.github.com>
2023-05-16 23:15:20 +0530

    Adding new plugin projects. (#5549)
    * Adding new plugin projects.
    
    * Plugin unpack
    
    * Removed spaces

---
+++ ALMAcceleratorSampleSolution/Plugins/PCATPluginTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PCATPluginTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PCATPluginTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d633d065-5775-48eb-ad30-e5758bc53dc3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
